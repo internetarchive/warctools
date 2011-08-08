2e9c0b6a83358be9d0d44293c19b043191ae1bdd
'Thomas Figg' <none@none>
2011-08-08 07:53:01 +0000

    init.py changes

--- warctools/__init__.py
+++ warctools/__init__.py
@@ -1,9 +1,13 @@
 from warctools.record import ArchiveRecord
 from warctools.warc import WarcRecord
 from warctools.arc import ArcRecord
+from warctools import record, warc, arc
 
 __all__= [
     'ArchiveRecord',
     'ArcRecord',
     'WarcRecord',
+    'record',
+    'warc',
+    'arc'
 ]
