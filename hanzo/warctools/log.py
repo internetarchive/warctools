c2143bfd17f40d5b86c8eb4c5ffe00eb09082a5b
Stephen Jones <stephen.jones@hanzoarchives.com>
2012-02-05 12:52:59 +0000

    Cleaner code that doesn't trigger a pylint error.

--- hanzo/warctools/log.py
+++ hanzo/warctools/log.py
@@ -2,8 +2,10 @@ import sys
 
 __all__ = ['debug']
 
-debug = lambda:None
-
 if __debug__:
     def debug(*args):
         print >> sys.stderr, 'WARCTOOLS',args
+else:
+    def debug(*args):
+        pass
+    
