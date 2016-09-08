fcd28f5b8191758da1644c576b05fe0062e76d03
Don Richards <highlander411@hotmail.com>
2016-09-08 13:14:51 -0400

    Normalizing '4.10.1dev2'
    python2.7 UserWarning: Normalizing '4.10.1dev2' to '4.10.1.dev2'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 setup(name='warctools',
-    version="4.10.1dev2",
+    version="4.10.1.dev2",
     license="MIT License",
     description='Command line tools and libraries for handling and manipulating WARC files (and HTTP contents)',
     author='Thomas Figg',
