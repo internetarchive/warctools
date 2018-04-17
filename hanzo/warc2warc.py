7b54c830cda78eae6ca57e9b40bad4cfd9bc8224
Christian Hecktor <christian.hecktor@arcor.de>
2018-04-17 15:39:21 +0200

    Enable -o parameter to write WARC data to a specific file

--- hanzo/warc2warc.py
+++ hanzo/warc2warc.py
@@ -3,11 +3,8 @@
 
 from __future__ import print_function
 
+import sys
 import os
-import sys
-
-import sys
-import os.path
 
 from optparse import OptionParser
 
@@ -17,18 +14,23 @@ from .httptools import RequestMessage, ResponseMessage
 parser = OptionParser(usage="%prog [options] url (url ...)")
 
 parser.add_option("-o", "--output", dest="output",
-                       help="output warc file")
+                  help="output warc file")
 parser.add_option("-l", "--limit", dest="limit")
 parser.add_option("-I", "--input", dest="input_format", help="(ignored)")
-parser.add_option("-Z", "--gzip", dest="gzip", action="store_true", help="compress output, record by record")
-parser.add_option("-D", "--decode_http", dest="decode_http", action="store_true", help="decode http messages (strip chunks, gzip)")
+parser.add_option("-Z", "--gzip", dest="gzip", action="store_true",
+                  help="compress output, record by record")
+parser.add_option("-D", "--decode_http", dest="decode_http",
+                  action="store_true", help="decode http messages (strip chunks, gzip)")
 parser.add_option("-L", "--log-level", dest="log_level")
-parser.add_option("--wget-chunk-fix", dest="wget_workaround", action="store_true", help="skip transfer-encoding headers in http records, when decoding them (-D)")
+parser.add_option("--wget-chunk-fix", dest="wget_workaround", action="store_true",
+                  help="skip transfer-encoding headers in http records, when decoding them (-D)")
 
-parser.set_defaults(output_directory=None, limit=None, log_level="info", gzip=False, decode_http=False, wget_workaround=False)
+parser.set_defaults(output_directory=None, limit=None, log_level="info",
+                    gzip=False, decode_http=False, wget_workaround=False)
 
 
 WGET_IGNORE_HEADERS = ['Transfer-Encoding']
+
 
 def process(record, out, options):
     ignore_headers = WGET_IGNORE_HEADERS if options.wget_workaround else ()
@@ -39,8 +41,9 @@ def process(record, out, options):
             if content_type == ResponseMessage.CONTENT_TYPE:
                 # technically, a http request needs to know the request to be parsed
                 # because responses to head requests don't have a body.
-                # we assume we don't store 'head' responses, and plough on 
-                message = ResponseMessage(RequestMessage(), ignore_headers=ignore_headers)
+                # we assume we don't store 'head' responses, and plough on
+                message = ResponseMessage(
+                    RequestMessage(), ignore_headers=ignore_headers)
             if content_type == RequestMessage.CONTENT_TYPE:
                 message = RequestMessage(ignore_headers=ignore_headers)
 
@@ -53,20 +56,37 @@ def process(record, out, options):
                 else:
                     error = []
                     if leftover:
-                        error.append("%d bytes unparsed"%len(leftover))
+                        error.append("%d bytes unparsed" % len(leftover))
                     if not message.complete():
-                        error.append("incomplete message (at %s, %s)"%(message.mode, message.header.mode))
-                    print('errors decoding http in record', record.id, ",".join(error), file=sys.stderr)
+                        error.append("incomplete message (at %s, %s)" %
+                                     (message.mode, message.header.mode))
+                    print('errors decoding http in record',
+                          record.id, ",".join(error), file=sys.stderr)
 
     record.write_to(out, gzip=options.gzip)
+
 
 def main(argv):
     (options, input_files) = parser.parse_args(args=argv[1:])
 
-    try: # python3
-        out = sys.stdout.buffer
-    except AttributeError: # python2
-        out = sys.stdout
+    if options.output is None:
+        # output file is not specified,
+        # therefore write to stdout
+        try:
+            # python3
+            out = sys.stdout.buffer
+        except AttributeError:
+            # python2
+            out = sys.stdout
+    else:
+        # create path to output file if necessary
+        output_file = options.output
+        output_directory = os.path.dirname(options.output)
+        if output_directory != "":
+            os.makedirs(output_directory, exist_ok=True)
+        # open output file
+        output_descriptor = open(output_file, "wb")
+        out = output_descriptor
 
     if len(input_files) < 1:
         fh = WarcRecord.open_archive(file_handle=sys.stdin, gzip=None)
@@ -81,15 +101,13 @@ def main(argv):
 
             fh.close()
 
-
-
+    out.close()
     return 0
+
 
 def run():
     sys.exit(main(sys.argv))
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     run()
-
-
