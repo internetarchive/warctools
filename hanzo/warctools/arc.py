eb38918b2a519d5419133d8add8c634f681752b6
Steve Sisney <steve@archive.org>
2019-12-11 01:50:11 +0000

    Added .pylintrc, and basic pylint fixes to arc.py

--- hanzo/warctools/arc.py
+++ hanzo/warctools/arc.py
@@ -1,5 +1,6 @@
-"""An object to represent arc records
-http://archive.org/web/researcher/ArcFileFormat.php
+"""
+ARC Record
+~~~~~~~~~~
 """
 
 import re
@@ -7,36 +8,41 @@ import re
 from hanzo.warctools.record import ArchiveRecord, ArchiveParser
 from hanzo.warctools.archive_detect import register_record_type
 
+
 # URL<sp>IP-address<sp>Archive-date<sp>Content-type<sp>
-#Result-code<sp>Checksum<sp>Location<sp> Offset<sp>Filename<sp>
-#Archive-length<nl> 
-# 
+# Result-code<sp>Checksum<sp>Location<sp> Offset<sp>Filename<sp>
+# Archive-length<nl>
+
 @ArchiveRecord.HEADERS(
-    URL = b'URL',
-    IP = b'IP-address',
-    DATE = b'Archive-date',
-    CONTENT_TYPE = b'Content-type',
-    CONTENT_LENGTH = b'Archive-length',
-    RESULT_CODE = b'Result-code',
-    CHECKSUM = b'Checksum',
-    LOCATION = b'Location',
-    OFFSET = b'Offset',
-    FILENAME = b'Filename',
+    URL=b'URL',
+    IP=b'IP-address',
+    DATE=b'Archive-date',
+    CONTENT_TYPE=b'Content-type',
+    CONTENT_LENGTH=b'Archive-length',
+    RESULT_CODE=b'Result-code',
+    CHECKSUM=b'Checksum',
+    LOCATION=b'Location',
+    OFFSET=b'Offset',
+    FILENAME=b'Filename',
 )
 class ArcRecord(ArchiveRecord):
+
+    """An object to represent arc records
+    http://archive.org/web/researcher/ArcFileFormat.php
+    """
 
     TRAILER = b'\n'  # an ARC record is trailed by single unix newline
 
     """Represents a record in an arc file."""
     def __init__(self, headers=None, content=None, errors=None):
-        ArchiveRecord.__init__(self, headers, content, errors) 
+        ArchiveRecord.__init__(self, headers, content, errors)
 
     @property
     def type(self):
         return b"response"
 
     def _write_to(self, out, nl):
-        #TODO: empty method?
+        # TODO: empty method?
         pass
 
     @classmethod
@@ -44,11 +50,12 @@ class ArcRecord(ArchiveRecord):
         """Constructs a parser for arc records."""
         return ArcParser()
 
+
 class ArcRecordHeader(ArcRecord):
     """Represents the headers in an arc record."""
     def __init__(self, headers=None, content=None, errors=None, version=None,
                  raw_headers=None):
-        ArcRecord.__init__(self, headers, content, errors) 
+        ArcRecord.__init__(self, headers, content, errors)
         self.version = version
         self.raw_headers = raw_headers
 
@@ -60,19 +67,21 @@ class ArcRecordHeader(ArcRecord):
         """Return the raw representation of this record."""
         return b"".join(self.raw_headers) + self.content[1]
 
+
 def rx(pat):
     """Helper function to compile a regular expression with the IGNORECASE
     flag."""
     return re.compile(pat, flags=re.IGNORECASE)
 
+
 nl_rx = rx('^\r\n|\r|\n$')
-length_rx = rx(b'^' + ArcRecord.CONTENT_LENGTH + b'$') #pylint: disable-msg=E1101
-type_rx = rx(b'^' + ArcRecord.CONTENT_TYPE + b'$')     #pylint: disable-msg=E1101
+length_rx = rx(b'^' + ArcRecord.CONTENT_LENGTH + b'$')
+type_rx = rx(b'^' + ArcRecord.CONTENT_TYPE + b'$')  # pylint: disable-msg=E1101
 SPLIT = re.compile(br'\b\s|\s\b').split
+
 
 class ArcParser(ArchiveParser):
     """A parser for arc archives."""
-
 
     def __init__(self):
         self.version = 0
@@ -82,16 +91,18 @@ class ArcParser(ArchiveParser):
 
         # question? will we get arc fragments?
         # should we store both headers & detect records by header length?
-        # if we don't know 
+        # if we don't know
 
         self.headers = []
 
     def parse(self, stream, offset, line=None):
         """Parses a stream as an arc archive and returns an Arc record along
         with the offset in the stream of the end of the record."""
+
         record = None
-        content_type = None
+        # content_type = None
         content_length = None
+
         if line is None:
             line = stream.readline()
 
@@ -115,18 +126,18 @@ class ArcParser(ArchiveParser):
             # configure parser instance
             self.version = arc_version.split()[0]
             self.headers = arc_names_line.strip().split()
-            
+
             # now we have read header field in record body
             # we can extract the headers from the current record,
             # and read the length field
 
             # which is in a different place with v1 and v2
-        
-            # read headers 
+
+            # read headers
             arc_headers = self.parse_header_list(line)
-            
+
             # extract content, ignoring header lines parsed already
-            content_type, content_length, errors = \
+            _content_type, content_length, errors = \
                 self.get_content_headers(arc_headers)
 
             content_length = content_length \
@@ -141,10 +152,10 @@ class ArcParser(ArchiveParser):
             if not self.headers:
                 raise Exception('missing filedesc')
             headers = self.parse_header_list(line)
-            content_type, content_length, errors = \
+            _content_type, content_length, errors = \
                 self.get_content_headers(headers)
 
-            record = ArcRecord(headers = headers, errors=errors)
+            record = ArcRecord(headers=headers, errors=errors)
 
         line = None
 
@@ -153,28 +164,29 @@ class ArcParser(ArchiveParser):
 
         return (record, (), offset)
 
-    def trim(self, stream):
-        return ()
-
     def parse_header_list(self, line):
+        """returns the list of record headers"""
+
         # some people use ' ' as the empty value. lovely.
         line = line.rstrip(b'\r\n')
         values = SPLIT(line)
         if len(self.headers) != len(values):
             if self.headers[0] in (ArcRecord.URL, ArcRecord.CONTENT_TYPE):
                 # fencepost
-                values = [s[::-1] for s in reversed(SPLIT(line[::-1], len(self.headers)-1))]
+                values = [s[::-1] for s in reversed(
+                    SPLIT(line[::-1], len(self.headers)-1))]
             else:
                 values = SPLIT(line, len(self.headers)-1)
 
         if len(self.headers) != len(values):
-            raise Exception('missing headers %s %s'%(",".join(values), ",".join(self.headers)))
-                
-        return list(zip(self.headers, values))
+            raise Exception('missing headers %s %s' % (
+                ",".join(values), ",".join(self.headers)))
 
+        return list(zip(self.headers, values))
 
     @staticmethod
     def get_content_headers(headers):
+        """returns content_type, content_length, errors from headers"""
         content_type = None
         content_length = None
         errors = []
